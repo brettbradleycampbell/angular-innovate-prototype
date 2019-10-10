61d23035a407c4825b6bdb35a2b31d21a94969b4
Brett Campbell <brettbradleycampbell@gmail.com>
2019-10-10 14:02:41 -0600

    Adding demo charts and controls to move, duplicate, or delete report elements.

--- src/app/demo-questions-minus-page-breaks.ts
+++ src/app/demo-questions-minus-page-breaks.ts
@@ -5,23 +5,14 @@ export const QUESTIONS: Question[] = [
     id: 1,
     questionText: 'What is your age?',
     questionGroup: 1,
-    questionType: 'multipleChoice',
-    answers: ['18 - 24', '25 - 34', '35 - 44', '45 - 54', '55+'],
-    skipLogic: true,
-    reportingVisualizationType: 'Multiple'
-  },
-  {
-    id: 2,
-    questionText: 'What state do you live in?',
-    questionGroup: 1,
     questionType: 'select',
-    answers: ['AL', 'AK', 'AZ', 'AR', 'CA - Long answer text and answer text and answer text and answer text and answer text and answer text and answer text and answer text and'],
+    answers: ['18 - 24', '25 - 34', '35 - 44', '45 - 54', '55+'],
     skipLogic2: true,
     reportingVisualizationType: 'Bar Chart'
   },
   {
-    id: 3,
-    questionText: 'How likely are you to recommend our business to a friend or colleague?',
+    id: 1,
+    questionText: 'What is your age?',
     questionGroup: 2,
     questionType: 'npm',
     displayLogic: true,
@@ -29,8 +20,8 @@ export const QUESTIONS: Question[] = [
     reportingVisualizationType: 'Line Graph'
   },
   {
-    id: 4,
-    questionText: 'Please rate your experience.',
+    id: 1,
+    questionText: 'What is your age?',
     questionGroup: 2,
     questionType: 'scale',
     answerElementType: 'stars',
@@ -38,19 +29,23 @@ export const QUESTIONS: Question[] = [
     reportingVisualizationType: 'Pie Chart'
   },
   {
-    id: 5,
+    id: 4,
     questionText: 'Please rate your experience.',
     questionGroup: 2,
     questionType: 'scale',
-    answerElementType: 'hearts',
+    answerElementType: 'stars',
+    answers: ['1', '2', '3', '4', '5'],
     skipLogicDestination2: true,
     reportingVisualizationType: 'Table'
   },
   {
-    id: 6,
-    questionText: 'Please rate your experience.',
-    questionGroup: 2,
-    questionType: 'scale',
-    answerElementType: 'numbers',
+    id: 1,
+    questionText: 'What is your age?',
+    questionGroup: 1,
+    questionType: 'multipleChoice',
+    answers: ['18 - 24', '25 - 34', '35 - 44', '45 - 54', '55+'],
+    skipLogic: true,
+    reportingVisualizationType: 'Multiple'
   },
+
 ];
